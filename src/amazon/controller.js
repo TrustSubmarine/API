5eac428a91978dd059c597a5ff0bddf5a08627c6
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2023-07-22 22:18:39 +0530

    Update addProd and updateProdById to setup scraper calls

--- src/amazon/controller.js
+++ src/amazon/controller.js
@@ -8,7 +8,9 @@ const getProdById = (req, res) => {
         if (error) {
             console.log(error);
         } else if (results.rows.length == 0) {
+            // TODO: Make a call to the scraper here
             res.status(404);
+            // TODO: Make a call to the post function below
         } else {
             res.json(results.rows);
         }
@@ -16,22 +18,33 @@ const getProdById = (req, res) => {
 };
 
 const addProd = (req, res) => {
-    const link_id = req.params.id;
-    const { name, link, score } = req.body; // is this safe?
-    pool.query(queries.addProd, [link_id, name, link, score], (error, results) => {
+    const link_id = req.params.link;
+    // TODO: Make a call to the scraper here and get everything
+    const { product_name, product_desc, score, is_calc } = req.body; // is this safe?
+    pool.query(queries.addProd, [link_id, product_name, product_desc, score, is_calc], (error, results) => {
         if (error) console.log(error);
         else res.send(`added ${link_id}`);
-    })
+    });
 };
 
 const updateProdById = (req, res) => {
-    const link_id = req.params.id;
+    const link_id = req.params.link;
+    // TODO: Make a call to the scraper to recalculate the score
     const { score } = req.body; // TODO: is this safe?
+    
+    pool.query(queries.getProdById, [link_id], (error, results) => {
+        if (error) {
+            console.log(error);
+        } else if (results.rows.length == 0) {
+            res.status(404);
+            res.send("Product does not exist");
+        }
+    });
+
     pool.query(queries.updateProdById, [score, link_id], (error, results) => {
         if (error) console.log(error);
-        // how to check if it exists already here? is this check needed?
         else res.send(`updated ${link_id}`);
-    })
+    });
 };
 
 const getAllProd = (req, res) => {
