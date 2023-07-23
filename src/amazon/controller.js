dd2100008aa28e31b0399a888af0f570425b7c05
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2023-07-23 17:38:10 +0530

    Add Scraper Integration to getProdById

--- src/amazon/controller.js
+++ src/amazon/controller.js
@@ -1,6 +1,9 @@
 const pool = require('../../db');
 // const createUnixSocketPool = require('../../connect-unix-pool')
 const queries = require('./queries');
+const axios = require('axios');
+
+const SCRAPER_BASE_ENDPOINT = "https://djrjzastptdwz2ddabucffyeka0mowff.lambda-url.eu-north-1.on.aws";
 
 const getProdById = (req, res) => {
     const link_id = decodeURIComponent(req.params.link);
@@ -8,9 +11,37 @@ const getProdById = (req, res) => {
         if (error) {
             console.log(error);
         } else if (results.rows.length == 0) {
-            // TODO: Make a call to the scraper here
-            res.status(404);
+            // Make a call to the scraper here
+            const linkUrlObject = new URL(SCRAPER_BASE_ENDPOINT);
+            const params = new URLSearchParams(linkUrlObject.search);
+            params.append("url", link_id);
+            linkUrlObject.search = params;
+
+            const reviews = axios.get(linkUrlObject.href)
+                .then(response => {
+                    // handle success
+                    console.log(response);
+                    return response;
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                    res.status(500).send("Internal Server Error: Invalid Scraper Response");
+                });
+
+            // TODO: send this to the model
+            // res.json(reviews);
+
+            // TODO: Send reviews to model
+
             // TODO: Make a call to the post function below
+            pool.query(queries.addProd, [encodeURIComponent(link_id), "prdo_name", "product_desco", 2.2, false], (error, results) => {
+                if (error) console.log(error);
+                else console.log(`added ${link_id}`);
+            });
+
+            res.json(reviews);
+
         } else {
             res.json(results.rows);
         }
